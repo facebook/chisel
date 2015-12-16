0511b9ae960b8a39089a19c2cca6a8cae57d662c
longv2go <long@fake.com>
2015-12-16 14:19:01 +0800

    typo fix

--- commands/FBClassDump.py
+++ commands/FBClassDump.py
@@ -29,9 +29,9 @@ class FBPrintMethods(fb.FBCommand):
   def run(self, arguments, options):
     cls = arguments[0]
     if not isClassObject(cls):
-        cls = runtimeHelpers.object_getClass(cls)
-        if not isClassObject(cls):
-            raise Exception('Invalid argument. Please specify an instance or a Class.')
+      cls = runtimeHelpers.object_getClass(cls)
+      if not isClassObject(cls):
+          raise Exception('Invalid argument. Please specify an instance or a Class.')
 
     if options.clsmethod:
       print 'Class Methods:'
@@ -48,22 +48,22 @@ class FBPrintMethods(fb.FBCommand):
       printInstanceMethods(cls, options.showaddr)
 
 def isClassObject(arg):
-    return runtimeHelpers.class_isMetaClass(runtimeHelpers.object_getClass(arg))
+  return runtimeHelpers.class_isMetaClass(runtimeHelpers.object_getClass(arg))
 
 def printInstanceMethods(cls, showaddr=False, prefix='-'):
-    json_method_array = get_oc_methods_json(cls)
-    if json_method_array:
-      for m in json_method_array:
-        method = Method(m)
+  json_method_array = get_oc_methods_json(cls)
+  if json_method_array:
+    for m in json_method_array:
+      method = Method(m)
 
-        if showaddr:
-          print prefix + ' ' + method.prettyPrintString() + ' ' + str(method.imp)
-        else:
-          print prefix + ' ' + method.prettyPrintString()
+      if showaddr:
+        print prefix + ' ' + method.prettyPrintString() + ' ' + str(method.imp)
+      else:
+        print prefix + ' ' + method.prettyPrintString()
 
 
 def printClassMethods(cls, showaddr=False):
-    printInstanceMethods(runtimeHelpers.object_getClass(cls), showaddr, '+')
+  printInstanceMethods(runtimeHelpers.object_getClass(cls), showaddr, '+')
 
 # Notice that evaluateExpression doesn't work with variable arguments. such as -[NSString stringWithFormat:]
 # I remove the "free(methods)" because it would cause evaluateExpressionValue to raise exception some time.
@@ -71,35 +71,34 @@ def get_oc_methods_json(klass):
   tmpString = """
     unsigned int outCount;
     Method *methods = (Method *)class_copyMethodList((Class)$cls, &outCount);
-    NSMutableArray *result2 = (id)[NSMutableArray array];
+    NSMutableArray *result = (id)[NSMutableArray array];
     
     for (int i = 0; i < outCount; i++) {
-         
-        NSMutableDictionary *m = (id)[NSMutableDictionary dictionary];
-        
-        SEL name = (SEL)method_getName(methods[i]);
-        [m setObject:(id)NSStringFromSelector(name) forKey:@"name"];
-        
-        char * ecodeing = (char *)method_getTypeEncoding(methods[i]);
-        [m setObject:(id)[NSString stringWithUTF8String:ecodeing] forKey:@"type_encoding"];
-        
-        NSMutableArray *types = (id)[NSMutableArray array];
-        NSInteger args = (NSInteger)method_getNumberOfArguments(methods[i]);
-        for (int idx = 0; idx < args; idx++) {
-            char *type = (char *)method_copyArgumentType(methods[i], idx);
-            [types addObject:(id)[NSString stringWithUTF8String:type]];
-        }
-        [m setObject:types forKey:@"parameters_type"];
-        
-        char *ret_type = (char *)method_copyReturnType(methods[i]);
-        [m setObject:(id)[NSString stringWithUTF8String:ret_type] forKey:@"return_type"];
-        
-        long imp = (long)method_getImplementation(methods[i]);
-        [m setObject:[NSNumber numberWithLongLong:imp] forKey:@"implementation"];
-        
-        [result2 addObject:m];
+      NSMutableDictionary *m = (id)[NSMutableDictionary dictionary];
+
+      SEL name = (SEL)method_getName(methods[i]);
+      [m setObject:(id)NSStringFromSelector(name) forKey:@"name"];
+      
+      char * encoding = (char *)method_getTypeEncoding(methods[i]);
+      [m setObject:(id)[NSString stringWithUTF8String:encoding] forKey:@"type_encoding"];
+      
+      NSMutableArray *types = (id)[NSMutableArray array];
+      NSInteger args = (NSInteger)method_getNumberOfArguments(methods[i]);
+      for (int idx = 0; idx < args; idx++) {
+          char *type = (char *)method_copyArgumentType(methods[i], idx);
+          [types addObject:(id)[NSString stringWithUTF8String:type]];
+      }
+      [m setObject:types forKey:@"parameters_type"];
+      
+      char *ret_type = (char *)method_copyReturnType(methods[i]);
+      [m setObject:(id)[NSString stringWithUTF8String:ret_type] forKey:@"return_type"];
+      
+      long imp = (long)method_getImplementation(methods[i]);
+      [m setObject:[NSNumber numberWithLongLong:imp] forKey:@"implementation"];
+      
+      [result addObject:m];
     }
-    RETURN(result2);
+    RETURN(result);
   """
   command = string.Template(tmpString).substitute(cls=klass)
   return fb.evaluate(command)
@@ -133,17 +132,17 @@ class Method:
   def __init__(self, json):
     self.name = json['name']
     self.type_encoding = json['type_encoding']
-    self.peremeters_type = json['parameters_type']
+    self.parameters_type = json['parameters_type']
     self.return_type = json['return_type']
     self.imp = self.toHex(json['implementation'])
 
   def prettyPrintString(self):
-    argnum = len(self.peremeters_type)
+    argnum = len(self.parameters_type)
     names = self.name.split(':')
 
     # the argnum count must be bigger then 2, index 0 for self, index 1 for SEL
     for i in range(2, argnum):
-      arg_type = self.peremeters_type[i]
+      arg_type = self.parameters_type[i]
       names[i-2] = names[i-2] + ":(" +  self.decode(arg_type) + ")arg" + str(i-2)
 
     string = " ".join(names)
@@ -161,3 +160,6 @@ class Method:
 
   def __str__(self):
     return "<Method:" + self.oc_method + "> " + self.name + " --- " + self.type + " --- " + self.imp
+
+
+# Properties
