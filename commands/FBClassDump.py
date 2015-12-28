6a3fb35755e627ad00e18b153957c1a8da813ad8
longv2go <long@fake.com>
2015-12-28 13:02:07 +0800

    Parser '@?' as a block in decode

--- commands/FBClassDump.py
+++ commands/FBClassDump.py
@@ -133,7 +133,9 @@ def decode(code):
   if code in encodeMap:
     ret = encodeMap[code]
   elif ret[0:1] == '@':
-    if ret[2:3] == '<':  # @"<aDelegate><bDelegate>"
+    if ret[1:2] == '?': # @? represent a block
+      ret = code
+    elif ret[2:3] == '<':  # @"<aDelegate><bDelegate>"
       ret = 'id' + ret[2:-1].replace('><', ', ')
     else:
       ret = ret[2:-1] + ' *'
