01d642687be044af9e7d8275786818e5db02b5de
Dave Lee <davelee.com@gmail.com>
2016-01-16 01:23:58 -0800

    Add function to enumerate all live memory allocations

---
+++ Chisel/CHLFunctions.h
@@ -0,0 +1,15 @@
+//
+//  CHLFunctions.h
+//  Chisel
+//
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <malloc/malloc.h>
+
+/**
+ * Enumerates all live memory allocations by calling `block` for each malloc range (represented as a `vm_range_t`).
+ */
+void CHLEnumerateAllocationsWithBlock(void (^block)(vm_range_t range));
