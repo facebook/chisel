4b59578e158775d0198ab755f2fd6302bb8e4fcb
Dave Lee <davelee.com@gmail.com>
2016-01-17 20:59:48 -0800

    Test that freshly malloc'd memory gets enumerated

--- ChiselTests/ChiselTests.m
+++ ChiselTests/ChiselTests.m
@@ -26,4 +26,22 @@
   XCTAssertTrue(seenSelf);
 }
 
+- (void)testAllocationsIncludesFreshMalloc
+{
+  u_int32_t size = MAX(arc4random_uniform(UINT8_MAX), 1);
+  void *memory = malloc(size);
+
+  __block BOOL seenMalloc = NO;
+  CHLEnumerateAllocationsWithBlock(^(vm_range_t range) {
+    if (range.address == (vm_address_t)memory) {
+      XCTAssertGreaterThanOrEqual(range.size, size);
+      seenMalloc = YES;
+    }
+  });
+
+  free(memory);
+
+  XCTAssertTrue(seenMalloc);
+}
+
 @end
