0fa54886d0e2115d0e641722a9f6e29916b5550f
Dave Lee <kastiglione@fb.com>
2016-07-21 21:26:07 -0700

    Add basic/default memory_reader_t implementation
    Not all malloc replacement have their own reader default.

--- Chisel/CHLAllocations.m
+++ Chisel/CHLAllocations.m
@@ -14,6 +14,12 @@
 // See vm_range_recorder_t
 static void enumerateRanges(task_t, void *, unsigned, vm_range_t *, unsigned);
 
+static kern_return_t reader(__unused task_t remote_task, vm_address_t remote_address, __unused vm_size_t size, void **local_memory)
+{
+  *local_memory = (void *)remote_address;
+  return KERN_SUCCESS;
+}
+
 void CHLEnumerateAllocationsWithBlock(void (^block)(vm_range_t))
 {
   /*
@@ -27,23 +33,17 @@ void CHLEnumerateAllocationsWithBlock(void (^block)(vm_range_t))
    *
    * Unfortunately, the memory allocations within a zone are not exposed directly, so the inner loop is not possible.
    * Instead, malloc zones provide a function that loops over the allocations, calling a callback function for each one.
-   */
-
-  /*
-   * === malloc.h API Notes ===
    *
-   * Both `malloc_get_all_zones` and `vm_range_recorder_t` callback functions take a `task_t` and a `memory_reader_t`.
-   * However it turns out both of these can be NULL. This information isn't documented in malloc.h but can be seen in
-   * the libmalloc source.
+   * Note:
    *
-   * `memory_reader_t`: When NULL, the fallback default is a memory reader which does not use the given `task_t`.
-   * `task_t`: Only required if used by a custom `memory_reader_t` or `vm_range_recorder_t` implementation.
+   * Both `malloc_get_all_zones` and `vm_range_recorder_t` callback functions take a `task_t` parameter. However it
+   * turns out it can be `NULL` provided it's not used by the `memory_reader_t` and `vm_range_recorder_t` functions.
    */
 
   // Get a reference to the array of zones.
   vm_address_t *zones;
   unsigned int zoneCount;
-  malloc_get_all_zones(TASK_NULL, NULL, &zones, &zoneCount);
+  malloc_get_all_zones(TASK_NULL, reader, &zones, &zoneCount);
 
   // Outer loop over the zones.
   for (unsigned int i = 0; i < zoneCount; i++) {
@@ -52,7 +52,7 @@ void CHLEnumerateAllocationsWithBlock(void (^block)(vm_range_t))
     malloc_zone_t *zone = (malloc_zone_t *)zones[i];
 
     // Inner loop over the zone's memory allocations.
-    zone->introspect->enumerator(TASK_NULL, block, MALLOC_PTR_IN_USE_RANGE_TYPE, (vm_address_t)zone, NULL, enumerateRanges);
+    zone->introspect->enumerator(TASK_NULL, block, MALLOC_PTR_IN_USE_RANGE_TYPE, (vm_address_t)zone, reader, enumerateRanges);
   }
 }
 
