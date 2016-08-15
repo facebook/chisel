d1a208490c70da5fab34beaafd4575576e1c1457
Dave Lee <kastiglione@fb.com>
2016-08-15 14:18:49 -0600

    Lazily check for +alloc overriding
    Instead of checking up front for +alloc overriding in every single class, this
    change makes the checking happen only on demand, and caches that result for
    future checks of the same class.

--- Chisel/CHLObjCObjectRecognizer.mm
+++ Chisel/CHLObjCObjectRecognizer.mm
@@ -12,42 +12,31 @@
 #import "CHLObjCObjectRecognizer.h"
 
 #include <unordered_map>
+#include <unordered_set>
 
 #include <objc/objc.h>
 #include <objc/runtime.h>
 
+@interface _CHLObjCAllocChecker : NSObject
+- (BOOL)isStandardAllocUsedByClass:(Class)cls;
+@end
+
 @implementation CHLObjCObjectRecognizer
 {
-  // This is primarily used for set lookup of classes, hence the name,
-  // but also records whether a class overrides +alloc/+allocWithZone:
-  std::unordered_map<Class, bool> _classSet;
-}
-
-static bool overridesAlloc(Class cls)
-{
-  static IMP objectAllocWithZone = NULL;
-  static IMP objectAlloc = NULL;
-  if (objectAllocWithZone == NULL || objectAlloc == NULL) {
-    const Class objectMetaClass = objc_getMetaClass("NSObject");
-    objectAllocWithZone = class_getMethodImplementation(objectMetaClass, @selector(allocWithZone:));
-    objectAlloc = class_getMethodImplementation(objectMetaClass, @selector(alloc));
-  }
-
-  const Class metaClass = object_getClass(cls);
-  return
-    class_getMethodImplementation(metaClass, @selector(allocWithZone:)) != objectAllocWithZone ||
-    class_getMethodImplementation(metaClass, @selector(alloc)) != objectAlloc;
+  std::unordered_set<Class> _classSet;
+  _CHLObjCAllocChecker *_allocChecker;
 }
 
 - (instancetype)init
 {
   self = [super init];
   if (self != nil) {
+    _allocChecker = [_CHLObjCAllocChecker new];
+
     unsigned int count;
     const auto classList = objc_copyClassList(&count);
     for (unsigned int i = 0; i < count; ++i) {
-      const auto cls = classList[i];
-      _classSet.emplace(cls, overridesAlloc(cls));
+      _classSet.insert(classList[i]);
     }
     free(classList);
   }
@@ -62,8 +51,7 @@ static bool overridesAlloc(Class cls)
   const auto cls = object_getClass(object);
 
   // The class/isa must be in the set of classes known by the runtime.
-  const auto entry = _classSet.find(cls);
-  if (entry == _classSet.cend()) {
+  if (_classSet.find(cls) == _classSet.cend()) {
     return NO;
   }
 
@@ -74,14 +62,48 @@ static bool overridesAlloc(Class cls)
   }
 
   // The memory size must either equal the instance size, or can be bigger if the class overrides +alloc.
-  const bool overridesAlloc = entry->second;
-  if (range.size > goodInstanceSize && !overridesAlloc) {
+  if (range.size > goodInstanceSize && [_allocChecker isStandardAllocUsedByClass:cls]) {
     return NO;
   }
 
   // TODO (maybe): Check that the ivars of object type ('@') point to valid objects, or are nil.
 
   return YES;
+}
+
+@end
+
+@implementation _CHLObjCAllocChecker
+{
+  IMP _objectAllocWithZone;
+  IMP _objectAlloc;
+  std::unordered_map<Class, bool> _cache;
+}
+
+- (instancetype)init
+{
+  self = [super init];
+  if (self != nil) {
+    const Class objectMetaClass = objc_getMetaClass("NSObject");
+    _objectAllocWithZone = class_getMethodImplementation(objectMetaClass, @selector(allocWithZone:));
+    _objectAlloc = class_getMethodImplementation(objectMetaClass, @selector(alloc));
+  }
+  return self;
+}
+
+- (BOOL)isStandardAllocUsedByClass:(Class)cls
+{
+  const auto match = _cache.find(cls);
+  if (match != _cache.cend()) {
+    return match->second;
+  }
+
+  const Class metaClass = object_getClass(cls);
+  const bool usesStandardAlloc =
+    class_getMethodImplementation(metaClass, @selector(allocWithZone:)) == _objectAllocWithZone &&
+    class_getMethodImplementation(metaClass, @selector(alloc)) == _objectAlloc;
+
+  return _cache[cls] = usesStandardAlloc;
 }
 
 @end
