4a7ace6f58925c9852565688bf078f08bc3b4291
Kolin Krewinkel <kkrewink@fb.com>
2019-09-24 23:56:51 -0700

    [Copyright] Add copyright header to all files, update to yearless variant in all files already containing a header

--- commands/FBClassDump.py
+++ commands/FBClassDump.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
 import string
 import lldb
 import fblldbbase as fb
